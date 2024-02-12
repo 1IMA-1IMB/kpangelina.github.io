ebabc575b043274104244a322b99223b1beb3e4a
Angelina Kaldbast <148325552+AngelinaKald@users.noreply.github.com>
2024-02-12 08:44:41 +0100

    multi converter

---
+++ multi_converter/index.js
@@ -0,0 +1,28 @@
+// let gal = 1;
+// let oz = 1;
+// let cup = 1;
+// let ss = 1;
+
+// const gallonToLiter = (gal) = gal * 3.78541;
+
+// const ouncesToLiter = (oz) = oz * 0.0295735;
+
+// const cupsToLiter = (cup) = cup * 0.236588;
+
+// const ssToLiter = (ss) = ss * 0.0147868;
+
+function liquidConverter(valNum) {
+    document.getElementById("outputGallon").innerHTML = valNum / 3.78541;
+}
+function liquidConverter(valNum) {
+    document.getElementById("outputOunce").innerHTML = valNum / 0.0295735;
+}
+function liquidConverter(valNum) {
+    document.getElementById("outputCup").innerHTML = valNum /  0.236588;
+}
+function liquidConverter(valNum) {
+    document.getElementById("outputSs").innerHTML = valNum /  0.0147868;
+}
+
+
+
