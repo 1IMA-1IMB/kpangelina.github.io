924a791e39398f4a1b768ae31cafaaed0fe26683
Angelina Kaldbast <148325552+AngelinaKald@users.noreply.github.com>
2024-02-14 09:37:24 +0100

    changes made to CSS

--- multi_converter/index.js
+++ multi_converter/index.js
@@ -1,27 +1,19 @@
-// let gal = 1;
-// let oz = 1;
-// let cup = 1;
-// let ss = 1;
-
-// const gallonToLiter = (gal) = gal * 3.78541;
-
-// const ouncesToLiter = (oz) = oz * 0.0295735;
-
-// const cupsToLiter = (cup) = cup * 0.236588;
-
-// const ssToLiter = (ss) = ss * 0.0147868;
-
 function liquidConverter(valNum) {
     document.getElementById("outputGallon").innerHTML = valNum / 3.78541;
-}
-function liquidConverter(valNum) {
     document.getElementById("outputOunce").innerHTML = valNum / 0.0295735;
-}
-function liquidConverter(valNum) {
     document.getElementById("outputCup").innerHTML = valNum /  0.236588;
-}
-function liquidConverter(valNum) {
     document.getElementById("outputSs").innerHTML = valNum /  0.0147868;
+}
+//palindrom oppg
+function palindromFunc(){
+    const palindrom = document.getElementById("palindromInput").value.toLowerCase()
+    const endret = palindrom.split("").reverse().join("")  
+ 
+    if (palindrom == endret){
+        document.getElementById("palindromOutput").innerHTML = "Palindrome"
+    } else {     
+        document.getElementById("palindromOutput").innerHTML = "Not a Palindrome"
+    }
 }
 
 
